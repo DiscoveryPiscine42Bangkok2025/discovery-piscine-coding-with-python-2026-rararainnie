2c421eca025031ba50b4853263e137fe5d0c50ac
Thumasorn Prasertsri <c.thumasorn@gmail.com>
2026-02-10 18:46:38 +0700

    [Finish] cell02

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
+sum = num1 * num2
+
+print(f"{num1} x {num2} = {sum}")
+
+if (sum == 0):
+    print("This result is positive and negative.")
+elif (sum > 0):
+    print("This result is positive.")
+else:
+    print("This result is negative.")
